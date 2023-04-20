be80f6e7c14b9f6349fe8b1f93f28982ac5f5aa9
Swaroop-Patil <74415510+Swaroop-Patil@users.noreply.github.com>
2023-04-20 20:13:53 +0530

    hopefully final ,config.smtp,config.port

--- config/config.js
+++ config/config.js
@@ -3,11 +3,15 @@ const emailUser="dtisswarooppatil@gmail.com";
 const emailPassword="dlsudqjjwyowgifb";
 const MONGO_URI='mongodb+srv://swaroop:ninjah2r@cluster0.han8srw.mongodb.net/REGISTRATIONS_JN?retryWrites=true&w=majority';
 
+const smtp='smtp.gmail.com';
+const port=578;
 
 module.exports ={
     sessionSecret,
     emailUser,
     emailPassword,
-    MONGO_URI
+    MONGO_URI,
+    smtp,
+    port
     
 }
